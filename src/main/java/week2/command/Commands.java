47acd2fe9ce4a94246a1667b3849815fa612636e
Juff <juff@ukr.net>
2015-09-17 08:01:09 +0300

    cls begining

--- src/main/java/week2/command/Commands.java
+++ src/main/java/week2/command/Commands.java
@@ -16,6 +16,65 @@ import java.util.HashMap;
 
 public class Commands {
 
+    public interface Command {
+        void execute();
+    }
+
+    public class ls implements Command{
+
+        @Override
+        public void execute() {
+
+        }
+    }
+
+    public class cd implements Command{
+
+        @Override
+        public void execute() {
+
+        }
+    }
+
+    public class mkdir implements Command{
+
+        @Override
+        public void execute() {
+
+        }
+    }
+
+    public class rm implements Command{
+
+        @Override
+        public void execute() {
+
+        }
+    }
+
+    public class cat implements Command{
+
+        @Override
+        public void execute() {
+
+        }
+    }
+
+    public class help implements Command{
+
+        @Override
+        public void execute() {
+            System.out.println("ls - вывести содержимое текущей директории\n" +
+                    "      cd - изменить текущую директорию\n" +
+                    "      mkdir - создать директорию\n" +
+                    "      rm - удалить файл или директорию\n" +
+                    "      cat - вывести содержимое файла\n" +
+                    "      help - доступные команды");
+        }
+    }
+
+
+
 
 
 }
---
+++ src/main/java/week2/command/Reciever.java
@@ -0,0 +1,8 @@
+package week2.command;
+
+/**
+ * Created by juff on 17.09.15.
+ */
+public class Reciever {
+    
+}
