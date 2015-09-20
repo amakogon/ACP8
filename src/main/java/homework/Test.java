4a0c18831b62821d3984dd695fb8fcb17c6a17bf
jed1knight <obonemax@gmail.com>
2015-09-20 15:01:11 +0300

    add class test

---
+++ src/main/java/homework/Test.java
@@ -0,0 +1,10 @@
+package homework;
+
+/**
+ * Created by Home on 20.09.2015.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
