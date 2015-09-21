506ff0268b5b88e2543c9798021063cc7ecf6332
Juff <juff@ukr.net>
2015-09-21 19:06:47 +0300

    another notebook

---
+++ src/main/java/week2/drinks/GUI.java
@@ -0,0 +1,16 @@
+package week2.drinks;
+
+import javax.swing.*;
+
+/**
+ */
+public class GUI {
+    public static void main(String[] args) {
+        //JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+
+        frame.setVisible(true);
+    }
+}
