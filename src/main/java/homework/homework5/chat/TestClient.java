a4c26bba8bd6995c8f6bd92187ad4781996bd90e
jed1knight <obonemax@gmail.com>
2015-10-12 21:25:21 +0300

    hg

---
+++ src/main/java/homework/homework5/chat/TestClient.java
@@ -0,0 +1,10 @@
+package homework.homework5.chat;
+
+/**
+ * Created by Home on 05.10.2015.
+ */
+public class TestClient {
+    public static void main(String[] args) {
+        new ChatClient();
+    }
+}
