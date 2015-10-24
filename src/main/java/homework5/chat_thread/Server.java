653e712f4513843642d364cb04aab67770483b69
Chernyshovdev <Chernyshov.dev@gmail.com>
2015-10-24 18:24:42 +0300

    add sendAll

--- src/main/java/homework5/chat_thread/Server.java
+++ src/main/java/homework5/chat_thread/Server.java
@@ -1,10 +1,13 @@
 package homework5.chat_thread;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -14,38 +17,41 @@ public class Server {
     public Server(int port) {
         this.port = port;
     }
+
     private int port;
     private ServerSocket serverSocket;
-    private Map<String, ServerSocket> clients;
+    private Map<String, Socket> clients;
 
     public void runServer() {
         clients = new HashMap<>();
-        String name=null;
+        String name;
         try {
-            serverSocket = new ServerSocket(5000);
+            serverSocket = new ServerSocket(port);
             while (true) {
                 Socket clientSock = serverSocket.accept();
                 PrintWriter writer = new PrintWriter(clientSock.getOutputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSock.getInputStream()));
                 writer.println("Connected to server!");
                 writer.println("Enter your nickname:");
-                clients.put(name,clientSock);
-                Thread clientThread = new Thread(new ReadTask(clientSock));
+                writer.flush();
+                name = reader.readLine();
+                clients.put(name, clientSock);
+                Thread clientThread = new Thread();
                 clientThread.start();
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    }
 
-    private boolean openConnection() {
-        try {
-            ServerSocket serverSocket = new ServerSocket(port);
-            socket=serverSocket.accept();
-        } catch (IOException e) {
-            e.printStackTrace();
+    private void sendAll(String message) {
+        Iterator iter = clients.entrySet().iterator();
+        while (iter.hasNext()) {
+            PrintWriter writer = (PrintWriter) iter.next();
+            writer.println(message);
+            writer.flush();
         }
-        return true;
     }
 }
+
 
